d4cbd75bafadd72f2bb609c8adb406520a1a9e78
Erik Nuber <erik.nuber@yahoo.com>
2024-08-11 15:54:11 -0700

    inital commit

---
+++ 1_SSH/1_intro.js
@@ -0,0 +1,31 @@
+// task
+// Set up ssh on your github account,
+
+// SSH - secure shell is a protocol. Way to contect two computers and have a shared agreement on how to communicate. SSH is a way for machines to communicate with one another.
+
+// HTTP - allows you to share send files over the internet bewteen browsers and servers
+// FTP - allows you to transfer files as well often used to upload files from your computer
+// HTTPS - similar to HTTP but is encrypted. It is "secure"
+
+// SSH - a shell needs a certain protocol to enable data exchange, it is also encrypted, between two devices not just a browser and a server. It is used over a shell. Talk to the operating system. You use the terminal to talk to another computer.
+
+// advantage of ssh is use of encryption between the host and the client. The host is the remote computer you are trying to acces and the client is your computer.
+
+// to do it, it is very simple to do in the terminal
+
+// ssh - key command instructs your system you want to open ane encrypted secure shell connection
+// user - represents the account you want to access.
+// host - refers to the computer to access, like an IP address or a domain name
+
+// ssh {user}@{host}
+
+// here root is used as the user, which may be a default in some cases and the IP address is the address for the host.
+// ex:  ssh root@167.99.146.57
+
+// when you do this, you will likely see something that changes your terminal to start with..
+
+// ex: root@ubuntu-s-1vcpu-1gb-nyc1-01:~#
+
+// this would replace your own location as you are now inside that other computer.
+
+// as with all terminals, same commands still work. (ls - list what ist here.)
